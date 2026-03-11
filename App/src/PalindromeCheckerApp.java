118a2920b35e18f92c161df46127cce59042630e
haripriya2007R <reddiharipriya933@gmail.com>
2026-03-11 15:47:55 +0530

    Update PalindromeCheckerApp.java

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,37 +1,32 @@
 
+import java.util.Scanner;
+
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
 
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
 
-                String input = "radar";
+        char[] chars = input.toCharArray();
+        int start = 0;
+        int end = chars.length - 1;
+        boolean isPalindrome = true;
 
-
-                char[] chars = input.toCharArray();
-
-
-                int start = 0;
-
-                int end = chars.length - 1;
-
-
-                boolean isPalindrome = true;
-
-
-                while (start < end) {
-
-                    if (chars[start] != chars[end]) {
-                        isPalindrome = false;
-                        break;
-                    }
-
-                    start++;
-                    end--;
-                }
-
-
-                System.out.println("Input : " + input);
-                System.out.println("Is Palindrome : " + isPalindrome);
+        while (start < end) {
+            if (chars[start] != chars[end]) {
+                isPalindrome = false;
+                break;
             }
+            start++;
+            end--;
         }
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome : " + isPalindrome);
+
+        sc.close();
+    }
+}
